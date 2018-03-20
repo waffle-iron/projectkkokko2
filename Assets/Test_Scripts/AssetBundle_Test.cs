786643ce04b5118dca7cacd840749e7ecf01a38e
Kevyn Montana <kevyn@ingenuity.ph>
2018-03-20 20:30:54 +0800

    *tested asset bundle code with reactive extensions and it works!

---
+++ Assets/Test_Scripts/AssetBundle_Test.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UniRx;
+using System;
+using System.Linq;
+using AssetBundles;
+using System.Threading;
+
+public class AssetBundle_Test : MonoBehaviour
+{
+    [SerializeField]
+    private string[] _bundles;
+
+    [SerializeField]
+    private string[] _objs;
+
+    [SerializeField]
+    List<GameObject> _pool = new List<GameObject>();
+
+    [SerializeField]
+    private List<AssetBundle> _loaded = new List<AssetBundle>();
+
+    private AssetBundleManager manager;
+
+    private void Start ()
+    {
+        //init manager
+        var loader = new AssetBundleLoader(_bundles, _objs);
+    }
+
+    
+}
