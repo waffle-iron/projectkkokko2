507f4737ae842afb424e58e32c4fdc0a399f84db
Kevyn Montana <kevyn@ingenuity.ph>
2018-03-22 00:36:37 +0800

    *added new config files for asset bundles, entities, and scene

---
+++ Assets/Sources/Configs/InitEntityConfig.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class InitEntityConfig : ScriptableObject
+{
+    [SerializeField]
+    private IEntityConfig[] entities;
+
+    public IEntityConfig[] Entities
+    {
+        get {
+            var newList = new IEntityConfig[entities.Length];
+            Array.Copy(entities, newList, entities.Length);
+            return newList;
+        }
+    }
+}
+
---
+++ Assets/Test_Scripts/AssetBundleLoader.cs
@@ -0,0 +1,115 @@
+﻿using AssetBundles;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using UniRx;
+using UnityEngine;
+
+public class AssetBundleLoader
+{
+    public AssetBundleLoader (string[] _bundles, string[] _objs)
+    {
+        Debug.Log("starting loader");
+        var timeStarted = Time.time;
+        Observable.FromCoroutine<AssetBundleManager>((observer, token) =>
+        {
+            return Init(null, observer, token);
+        })
+            .Where(manager => manager != null)
+            .SelectMany(manager =>
+            {
+                //get bundles
+                return Observable.FromCoroutine<AssetBundle>((observer, token) =>
+                LoadBundle(manager, _bundles, observer, token));
+            })
+            .Where(bundle => bundle != null)
+            .Select(bundle =>
+            {
+                Debug.Log($"bundled loaded: {bundle.name} - {Time.time - timeStarted}");
+                return LoadAsset<GameObject>(bundle, _objs);
+            })
+            .Merge()
+            .Subscribe(obj =>
+            {
+                //get objects
+                Debug.Log($"obj loaded: {obj.name} - {Time.time - timeStarted}");
+                GameObject.Instantiate(obj);
+            });
+    }
+
+    IEnumerator Init (string uri, IObserver<AssetBundleManager> observer, CancellationToken token)
+    {
+        var manager = new AssetBundleManager();
+        manager.SetPrioritizationStrategy(AssetBundleManager.PrioritizationStrategy.PrioritizeStreamingAssets);
+        if (uri != null) { manager.SetBaseUri(uri); }
+        else { manager.UseStreamingAssetsFolder(); }
+
+        var asyncInit = manager.InitializeAsync();
+        yield return asyncInit;
+
+        if (token.IsCancellationRequested) { yield break; }
+
+        if (asyncInit.Success == false) { observer.OnError(new Exception("initialization failed;")); }
+        else
+        {
+            Debug.Log("loaded manager");
+            observer.OnNext(manager);
+            observer.OnCompleted();
+        }
+    }
+
+    IEnumerator LoadBundle (AssetBundleManager manager, string bundle, IObserver<AssetBundle> observer, CancellationToken token)
+    {
+        return LoadBundle(manager, new string[] { bundle }, observer, token);
+    }
+    IEnumerator LoadBundle (AssetBundleManager manager, string[] bundles, IObserver<AssetBundle> observer, CancellationToken token)
+    {
+        foreach (var bundle in bundles)
+        {
+            var loadedBundle = manager.GetBundleAsync(bundle);
+            yield return loadedBundle;
+
+            if (token.IsCancellationRequested) { yield break; }
+
+            if (loadedBundle.AssetBundle == null) { Debug.LogError($"bundle {bundle} not found"); }
+            else
+            {
+                Debug.Log($"loaded bundle {loadedBundle.AssetBundle.name}");
+                observer.OnNext(loadedBundle.AssetBundle);
+            }
+
+            if (token.IsCancellationRequested) { yield break; }
+        }
+        observer.OnCompleted();
+    }
+
+    IObservable<T> LoadAsset<T> (AssetBundle bundle, string[] names) where T : UnityEngine.Object
+    {
+        var requests = new List<AssetBundleRequest>();
+
+        foreach (var name in names)
+        {
+            requests.Add(bundle.LoadAssetAsync<T>(name));
+        }
+
+        return requests.ToObservable()
+                    .Select(request => request.AsAsyncOperationObservable())
+                    .Merge()
+                    .Where(request => request.isDone)
+                    .Select(request => request.asset as T);
+    }
+    IObservable<T> LoadAllAssets<T> (AssetBundle bundle) where T : UnityEngine.Object
+    {
+        var loadStream = bundle.LoadAllAssetsAsync<T>().AsAsyncOperationObservable()
+                            .Where(request => request.isDone)
+                            .SelectMany(request =>
+                            {
+                                return request.allAssets.Select(obj => obj as T).ToArray();
+                            });
+
+        return loadStream;
+    }
+}
+
