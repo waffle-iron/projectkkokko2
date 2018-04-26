29d496ef99bc610dc8b119c44c9494d3a25ba8e3
Kevyn Montana <kevyn@ingenuity.ph>
2018-04-27 04:09:27 +0800

    added load apartment items placement

--- Assets/Sources/Configs/Resources/Items/Apartment Stuff/ApartmentInstanceDataConfig.cs
+++ Assets/Sources/Configs/Resources/Items/Apartment Stuff/ApartmentInstanceDataConfig.cs
@@ -7,11 +7,15 @@ using UniRx;
 public class ApartmentInstanceDataConfig : UnityEntityConfig
 {
     //enter serialized fields here
+    [Header("Apartment Instance Data Settings")]
+    [SerializeField, SceneName]
+    private List<string> _scenesToLoad;
 
     protected override IEntity CustomCreate (Contexts contexts)
     {
         var gameEty = contexts.game.CreateEntity();
         gameEty.AddApartmentItemsInstanceData(new Dictionary<string, Dictionary<string, Vector3>>());
+        gameEty.AddScenes(_scenesToLoad);
         gameEty.isDoNotDestroyOnSceneChange = true;
         return gameEty;
     }
